dbc8b0abbf03c42f583248170693c4a7a4f7fc8c
magicbett <stanley.kariba@yahoo.com>
2022-09-20 22:46:46 +0300

    2nd assignment of python is awesome

--- 0x01-python-if_else_loops_functions/102-magic_calculation.py
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python3
-def magic_calculation(a, b):
-    return (98 + a ** b)
+def magic_calculation(a, b, c):
+    if a < b:
+        return c
+    else:
+        if c > b:
+            return a + b
+        return (a * b) - c
